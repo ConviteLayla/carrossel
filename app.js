d3333a19aa454f53c8f118f7ee68e020436a7782
katrimor <kauam08000@gmail.com>
2024-11-27 12:03:29 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const persons = [
     {
         photo: "person-1.jpeg",
-        nome: "Paola Eduarda",
-        trabalho: "Desenvolvedora Front-End",
-        descricao: "Paola é uma desenvolvedora apaixonada por criar interfaces de usuário intuitivas e responsivas."
+        author: "Paola Eduarda",
+        job: "Desenvolvedora Front-End",
+        info: "Paola é uma desenvolvedora apaixonada por criar interfaces de usuário intuitivas e responsivas."
     },
     {   
         photo: "https://images.pexels.com/photos/7562313/pexels-photo-7562313.jpeg",
-        nome: "Carlos Silva",
-        trabalho: "Designer Gráfico",
-        descricao: "Carlos é um designer criativo com experiência em branding e design de produtos."
+        author: "Carlos Silva",
+        job: "Designer Gráfico",
+        info: "Carlos é um designer criativo com experiência em branding e design de produtos."
     },
     {
         photo: "https://images.pexels.com/photos/4350178/pexels-photo-4350178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        nome: "Maria Oliveira",
-        trabalho: "Gerente de Projetos",
-        descricao: "Maria é uma gerente de projetos experiente, especializada em metodologias ágeis."
+        author: "Maria Oliveira",
+        job: "Gerente de Projetos",
+        info: "Maria é uma gerente de projetos experiente, especializada em metodologias ágeis."
     },
     {
         photo: "https://images.pexels.com/photos/6652928/pexels-photo-6652928.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        nome: "João Pereira",
-        trabalho: "Analista de Dados",
-        descricao: "João é um analista de dados que adora transformar dados brutos em insights valiosos."
+        author: "João Pereira",
+        job: "Analista de Dados",
+        info: "João é um analista de dados que adora transformar dados brutos em insights valiosos."
     }
 ];
 
@@ -29,55 +29,32 @@ const photo = document.querySelector(".person-image")
 const author = document.querySelector(".author")
 const job = document.querySelector(".job")
 const info = document.querySelector(".info")
+const btn = document.querySelectorAll("#prev-btn")
 
-window.addEventListener("DOMContentLoaded", () => {
-    photo.setAttribute("src", persons[index].photo)
-    author.textContent = persons[index].nome
-    job.textContent = persons[index].trabalho
-    info.textContent = persons[index].descricao
-    document.querySelector(".before").style.color = "#00437a7a"
-})
+let index = 0
+function loadProfile(person){
+    photo.setAttribute("src", persons[person].photo)
+    author.textContent = persons[person].author
+    job.textContent = persons[person].job 
+    info.textContent = persons[person].info
 
-const btn = document.querySelectorAll(".prev-btn")
-index = 0
-btn.forEach((itens) => {
-    itens.addEventListener("click", (event) =>  {
-        if (event.target.classList == "prev-next") {
-            if (index >= 0 && index < persons.length - 1) {
-                index++
-                photo.setAttribute("src", persons[index].photo)
-                author.textContent = persons[index].nome
-                job.textContent = persons[index].trabalho
-                info.textContent = persons[index].descricao
-                document.querySelector(".before").style.color = "#00437a"
-                if (index == 3) {
-                document.querySelector(".next").style.color = "#00437a7a"
+    document.querySelector(".next").style.color = person == persons.length - 1 ? "#00437a23" : "#00437a"
+    document.querySelector(".before").style.color = person == 0 ? "#00437a23" : "#00437a"
+}
 
-                }
+window.addEventListener("DOMContentLoaded", loadProfile(index))
 
-            }
-            else{
-                index = persons.length - 1 
-                
-            }
+btn.forEach((item) => {
+    item.addEventListener("click", (event) => {
+        if (event.currentTarget.classList.contains("next")){
+            index++
+            index = index > persons.length - 1 ? persons.length - 1 : index
+            loadProfile(index)
         }
-        else if (event.target.classList == "prev-before") {
-            if (index > 0){
-                index--
-                photo.setAttribute("src", persons[index].photo)
-                author.textContent = persons[index].nome
-                job.textContent = persons[index].trabalho
-                info.textContent = persons[index].descricao
-                document.querySelector(".next").style.color = "#00437a"
-                if (index == 0) {
-                    document.querySelector(".before").style.color = "#00437a7a"
-
-                }
-            }
-            else {
-                index = 0
-            }
+        else if (event.currentTarget.classList.contains("before")){
+            index--
+            index = index < 0 ? 0 : index
+            loadProfile(index)
         }
     })
-
 })
